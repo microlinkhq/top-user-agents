d94adade3e2b2f7808b10423fb5d744c3eaf4475
Sayem Chowdhury <sayem314@gmail.com>
2020-05-09 21:48:15 +0600

    improvement: replace cheerio with node-html-parser

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,26 +1,22 @@
-"use strict";
-const writeJsonFile = require("write-json-file");
-const cheerio = require("cheerio");
-const got = require("hooman");
+'use strict'
+const got = require('hooman')
+const { parse } = require('node-html-parser')
+const writeJsonFile = require('write-json-file')
 
 const main = async () => {
   try {
     // prettier-ignore
     const { body } = await got("https://techblog.willshouse.com/2012/01/03/most-common-user-agents/");
 
-    const $ = cheerio.load(body);
+    // User-Agents
+    const userAgents = parse(body).querySelector('.get-the-list').text
 
-    const userAgents = $("tbody .useragent")
-      .map(function() {
-        return $(this).text();
-      })
-      .get();
-
-    await writeJsonFile("index.json", userAgents);
+    // Save to file
+    await writeJsonFile('index.json', userAgents.split('\n'))
   } catch (err) {
-    console.error(err);
-    process.exit(1);
+    console.error(err)
+    process.exit(1)
   }
-};
+}
 
-main();
+main()
